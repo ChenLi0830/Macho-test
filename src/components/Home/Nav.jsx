9a4780e0ca80c6c8057354bb65382e6d775e4ded
Chen Li <yichen.li0830@gmail.com>
2017-02-09 13:14:09 -0800

    Nav section

--- src/components/Home/Nav.jsx
+++ src/components/Home/Nav.jsx
@@ -13,74 +13,80 @@ const Header = (props) => {
   
   let navData = {menu1: 'PRODUCT', menu2: 'WORKS', menu3: 'TEAM', menu4: 'ABOUT'};
   const navChildren = Object.keys(navData)
-      .map((key, i) => (<Item key={i}>{navData[key]}</Item>));
+      .map((key, i) => (<Item style={{borderWidth: 0}} key={i}>{navData[key]}</Item>));
   navChildren.push(<Item key={4} className={"contact-btn"}>{'CONTACT'}</Item>);
   
   navData = {...navData, menu5: "CONTACT"};
   const dropDownChildren = Object.keys(navData).map((key, i) => (
-      <Item key={i}>{navData[key]}</Item>
+          <Item key={i}>{navData[key]}</Item>
       )
   );
   
   return (<Row>
     <TweenOne
-      component="header"
-      animation={{opacity: 0, type: 'from'}}
-      id={props.id}
-      className={props.className}
-      style={{backgroundColor: 'rgba(255, 255, 255, 1)'}}
-  >
-    <TweenOne
-        className={`header0-logo`}
-        animation={{x: -30, type: 'from', ease: 'easeOutQuad'}}
+        component="header"
+        animation={{opacity: 0, type: 'from'}}
+        id={props.id}
+        className={props.className}
+        style={{backgroundColor: 'rgba(255, 255, 255, 1)'}}
     >
-      <h1>
-        MACHO
-      </h1>
-      {/*<img width="100%" src="https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg" />*/}
-    </TweenOne>
-    <Col xs={{span: 0}} sm={{span: 20, offset: 4}} md={{span: 16, offset: 8}}>
       <TweenOne
-          className={`header0-nav`}
-          animation={{x: 30, type: 'from', ease: 'easeOutQuad'}}
+          className={`header0-logo`}
+          animation={{x: -30, type: 'from', ease: 'easeOutQuad'}}
       >
-        <Menu
-            mode="horizontal"
-            style={{color: '#c1c1c1'}}
-            selectedKeys={[String(props.tab)]}
-            onClick = {({item, key, keyPath})=>{props.changeTab(key)}}
+        <a href="#">
+          <h1>
+            MACHO
+          </h1>
+        </a>
+        {/*<img width="100%" src="https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg" />*/}
+      </TweenOne>
+      <Col xs={{span: 0}} sm={{span: 20, offset: 4}} md={{span: 16, offset: 8}}>
+        <TweenOne
+            className={`header0-nav`}
+            animation={{x: 30, type: 'from', ease: 'easeOutQuad'}}
         >
-          {navChildren}
-        </Menu>
-      </TweenOne>
-    </Col>
-    
-    <Col xs={{span: 6, offset: 18}} sm={{span: 0}}>
-      <TweenOne
-          className={`header0-nav`}
-          animation={{x: 30, type: 'from', ease: 'easeOutQuad'}}
-      >
-        <Icon type="bars" className="header0-toggle" onClick={() => {
-          props.toggle()
-        }}/>
-      </TweenOne>
-    </Col>
-    
-    {
-      props.showDropDown
-      &&
+          <Menu
+              mode="horizontal"
+              style={{color: '#c1c1c1'}}
+              selectedKeys={[String(props.tab)]}
+              onClick={({item, key, keyPath}) => {
+                props.changeTab(key)
+              }}
+          >
+            {navChildren}
+          </Menu>
+        </TweenOne>
+      </Col>
+      
+      <Col xs={{span: 6, offset: 18}} sm={{span: 0}}>
+        <TweenOne
+            className={`header0-nav`}
+            animation={{x: 30, type: 'from', ease: 'easeOutQuad'}}
+        >
+          <Icon type="bars" className="header0-toggle" onClick={() => {
+            props.toggle()
+          }}/>
+        </TweenOne>
+      </Col>
+      
+      {
+        props.showDropDown
+        &&
         <Menu
             style={{width: "100%"}}
             defaultOpenKeys={['sub1']}
             mode="vertical"
             selectedKeys={[String(props.tab)]}
-            onClick = {({item, key, keyPath})=>{props.changeTab(key)}}
+            onClick={({item, key, keyPath}) => {
+              props.changeTab(key)
+            }}
         >
           {dropDownChildren}
         </Menu>
-    }
-  
-  </TweenOne>
+      }
+    
+    </TweenOne>
   </Row>);
 };
 
