72d3a04417ed8021a2fb00c3f9fd0f9731339d86
Chen Li <yichen.li0830@gmail.com>
2017-02-09 12:02:53 -0800

    Nav use bootstrap

--- src/components/Home/Nav.jsx
+++ src/components/Home/Nav.jsx
@@ -1,40 +1,118 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import TweenOne from 'rc-tween-one';
-import { Menu } from 'antd';
+import {Menu, Col, Icon, Row} from 'antd';
+import {connect} from 'react-redux';
+import {navActions} from '../../modules';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+import { Collapse } from 'antd';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 const Item = Menu.Item;
 
-class Header extends React.Component {
-  render() {
-    const navData = { menu1: '导航一', menu2: '导航二', menu3: '导航三', menu4: '导航四' };
-    const navChildren = Object.keys(navData)
+const Header = (props) => {
+  console.log("props", props);
+  
+  let navData = {menu1: 'PRODUCT', menu2: 'WORKS', menu3: 'TEAM', menu4: 'ABOUT'};
+  const navChildren = Object.keys(navData)
       .map((key, i) => (<Item key={i}>{navData[key]}</Item>));
-    return (<TweenOne
+  navChildren.push(<Item key={4} className={"contact-btn"}>{'CONTACT'}</Item>);
+  
+  navData = {...navData, menu5: "CONTACT"};
+  const dropDownChildren = Object.keys(navData).map((key, i) => (
+      <Item key={i}>{navData[key]}</Item>
+      )
+  );
+  
+  // return   <Navbar inverse collapseOnSelect>
+  //   <Navbar.Header>
+  //     <Navbar.Brand>
+  //       <a href="#">React-Bootstrap</a>
+  //     </Navbar.Brand>
+  //     <Navbar.Toggle />
+  //   </Navbar.Header>
+  //   <Navbar.Collapse>
+  //     <Nav>
+  //       <NavItem eventKey={1} href="#">Link</NavItem>
+  //       <NavItem eventKey={2} href="#">Link</NavItem>
+  //       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+  //         <MenuItem eventKey={3.1}>Action</MenuItem>
+  //         <MenuItem eventKey={3.2}>Another action</MenuItem>
+  //         <MenuItem eventKey={3.3}>Something else here</MenuItem>
+  //         <MenuItem divider />
+  //         <MenuItem eventKey={3.3}>Separated link</MenuItem>
+  //       </NavDropdown>
+  //     </Nav>
+  //     <Nav pullRight>
+  //       <NavItem eventKey={1} href="#">Link Right</NavItem>
+  //       <NavItem eventKey={2} href="#">Link Right</NavItem>
+  //     </Nav>
+  //   </Navbar.Collapse>
+  // </Navbar>;
+  
+  return (<Row>
+    <TweenOne
       component="header"
-      animation={{ opacity: 0, type: 'from' }}
-      {...this.props}
-      style={{ backgroundColor: 'rgba(255, 255, 255, 0.95)' }}
+      animation={{opacity: 0, type: 'from'}}
+      id={props.id}
+      className={props.className}
+      style={{backgroundColor: 'rgba(255, 255, 255, 1)'}}
+  >
+    <TweenOne
+        className={`header0-logo`}
+        animation={{x: -30, type: 'from', ease: 'easeOutQuad'}}
     >
+      <h1>
+        MACHO
+      </h1>
+      {/*<img width="100%" src="https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg" />*/}
+    </TweenOne>
+    <Col xs={0} sm={16} md={12} lg={8}>
       <TweenOne
-        className={`${this.props.className}-logo`}
-        animation={{ x: -30, type: 'from', ease: 'easeOutQuad' }}
-      >
-        <img width="100%" src="https://os.alipayobjects.com/rmsportal/mlcYmsRilwraoAe.svg" />
-      </TweenOne>
-      <TweenOne
-        className={`${this.props.className}-nav`}
-        animation={{ x: 30, type: 'from', ease: 'easeOutQuad' }}
+          className={`header0-nav`}
+          animation={{x: 30, type: 'from', ease: 'easeOutQuad'}}
       >
         <Menu
-          mode="horizontal" defaultSelectedKeys={['a']}
-          style={{ color: '#c1c1c1' }}
+            mode="horizontal"
+            style={{color: '#c1c1c1'}}
+            selectedKeys={[String(props.tab)]}
+            onClick = {({item, key, keyPath})=>{props.changeTab(key)}}
         >
           {navChildren}
         </Menu>
       </TweenOne>
-    </TweenOne>);
-  }
-}
+    </Col>
+
+    <Col xs={{span: 6, offset: 18}} sm={{span: 0}}>
+      <TweenOne
+          className={`header0-nav`}
+          animation={{x: 30, type: 'from', ease: 'easeOutQuad'}}
+      >
+        <Icon type="bars" style={{fontSize: 20, cursor: "pointer"}} onClick={() => {
+          props.toggle()
+        }}/>
+      </TweenOne>
+    </Col>
+
+    {
+      props.showDropDown
+      &&
+      <div>
+        <Menu
+            style={{width: "100%", display: "block"}}
+            defaultOpenKeys={['sub1']}
+            mode="vertical"
+            selectedKeys={[String(props.tab)]}
+            onClick = {({item, key, keyPath})=>{props.changeTab(key)}}
+        >
+          {dropDownChildren}
+        </Menu>
+      </div>
+    }
+
+  </TweenOne>
+  </Row>);
+};
 
 Header.propTypes = {
   className: PropTypes.string,
@@ -44,4 +122,24 @@ Header.defaultProps = {
   className: 'header0',
 };
 
-export default Header;
+// Container
+const mapStateToProps = (state) => {
+  // console.log("state", state);
+  return {
+    tab: state.nav.tab,
+    showDropDown: state.nav.showDropDown,
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggle: () => {
+      dispatch(navActions.navToggleMenu())
+    },
+    changeTab: (tab) => {
+      dispatch(navActions.navChangeTab(tab))
+    },
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
