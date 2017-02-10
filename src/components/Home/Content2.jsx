0e029031510b15b1522cfdacd3cbc4587e9bff79
Chen Li <yichen.li0830@gmail.com>
2017-02-09 22:12:51 -0800

    Implementing product page

--- src/components/Home/Content2.jsx
+++ src/components/Home/Content2.jsx
@@ -1,63 +1,110 @@
 import React from 'react';
+import {Col, Row} from 'antd';
+import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
 import QueueAnim from 'rc-queue-anim';
 import TweenOne from 'rc-tween-one';
-import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
 
-class Content extends React.Component {
+// const Content = (props) => {
+//   return (
+//       <div
+//           {...props}
+//           className="content-template-wrapper content-half-wrapper"
+//       >
+//         <OverPack
+//             className={`content-template content1`}
+//             hideProps={{img: {reverse: true}}}
+//             location={props.id}
+//         >
+//           <QueueAnim
+//               type="left"
+//               className={`content1-text`}
+//               key="text"
+//               leaveReverse
+//               ease={['easeOutCubic', 'easeInCubic']}
+//           >
+//             <Col xs={{span: 10}}>
+//               <h1>
+//                 The Macho App
+//               </h1>
+//               <p>
+//                 Qué es la mente de principiante? Está dejando atrás nuestras expectativas y
+//                 ideas
+//                 preconcebidas acerca de algo, y ver cosas con una mente abierta, ojos frescos,
+//                 como un principiante. </p>  <p> Si alguna vez has aprendido algo nuevo, puedes
+//               recordar lo que
+//               es eso: probablemente estás confundido, porque no sabes cómo hacer lo que estás
+//               aprendiendo </p>
+//             </Col> </QueueAnim> <TweenOne key="img"
+//                                           animation={{x: '+=30', opacity: 0, type: 'from'}}
+//                                           className={`content1-img`}> <Col
+//             xs={{span: 14, offset: 10}}>
+//           <span>
+//             <img width="100%"
+//                  src="https://s3-us-west-2.amazonaws.com/macho.test/images/iPhone_6.png"/>
+//           </span>
+//         </Col>
+//         </TweenOne> </OverPack></div> );
+// };
 
-  static propTypes = {
-    id: React.PropTypes.string,
-    className: React.PropTypes.string,
-  };
 
-  static defaultProps = {
-    className: 'content1',
-  };
-
-  render() {
-    return (
+const Content = (props) => {
+  console.log("props", props);
+  return (
       <div
-        {...this.props}
-        className="content-template-wrapper content-half-wrapper"
+          {...props}
+          className="content-template-wrapper content-half-wrapper2"
       >
-        <OverPack
-          className={`content-template ${this.props.className}`}
-          hideProps={{ img: { reverse: true } }}
-          location={this.props.id}
-        >
-          <QueueAnim
-            type="left"
-            className={`${this.props.className}-text`}
-            key="text"
-            leaveReverse
-            ease={['easeOutCubic', 'easeInCubic']}
-          >
-            <h1
-              key="h1"
+        
+      <OverPack location={props.id}
+                hideProps={{ img: { reverse: true } }}
+                className={`content-template`}>
+        {/*<Row style={{display:"flex", alignItems:"center"}}>*/}
+          <Col xs={{span: 10, offset: 2}}>
+            <TweenOne
+                key="img"
+                animation={{ x: '-=30', opacity: 0, type: 'from' }}
             >
-              分布式中间件
+            <h1>
+              The Macho App
             </h1>
-            <p
-              key="p"
-            >
-              金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。
+            <p>
+              Qué es la mente de principiante? Está dejando atrás nuestras expectativas y ideas
+              preconcebidas acerca de algo, y ver cosas con una mente abierta, ojos frescos, como
+              un
+              principiante.
             </p>
-          </QueueAnim>
-          <TweenOne
-            key="img"
-            animation={{ x: '+=30', opacity: 0, type: 'from' }}
-            className={`${this.props.className}-img`}
-          >
-            <span
+        
+            <p>
+              Si alguna vez has aprendido algo nuevo, puedes recordar lo que es eso:
+              probablemente estás confundido, porque no sabes cómo hacer lo que estás aprendiendo
+            </p>
+            </TweenOne>
+          </Col>
+        
+          <Col xs={{span: 10}}>
+            <TweenOne
+                key="text"
+                animation={{ x: '+=30', opacity: 0, type: 'from', delay:'100'}}
             >
-              <img width="100%" src="https://zos.alipayobjects.com/rmsportal/tvQTfCupGUFKSfQ.png" />
+            <span>
+              <img width="100%"
+                 src="https://s3-us-west-2.amazonaws.com/macho.test/images/iPhone_6.png"/>
             </span>
-          </TweenOne>
-        </OverPack>
+            </TweenOne>
+          </Col>
+        {/*</Row>*/}
+      </OverPack>
       </div>
-    );
-  }
-}
+  );
+};
 
+Content.propTypes = {
+  id: React.PropTypes.string,
+  className: React.PropTypes.string,
+};
+
+Content.defaultProps = {
+  className: 'content1',
+};
 
 export default Content;
