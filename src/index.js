02ee8edf61b33fb211e3626e02621d16310f5553
Chen Li <yichen.li0830@gmail.com>
2017-02-10 10:10:11 -0800

    Wrap router with redux store provider

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import {render} from 'react-dom';
 import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import Home from './components/Home/index.jsx';
 import 'babel-polyfill';
-// import {Provider} from 'react-redux';
-// import store from './modules/index';
+import {Provider} from 'react-redux';
+import store from '../src/modules/index';
 // import AppContainer from './Components/AppContainer';
 import PageNotFound from './components/PageNotFound';
 
@@ -15,11 +15,11 @@ const router = (
     </Router>
 );
 
-render(router, document.getElementById('app'));
+// render(router, document.getElementById('app'));
 
-// render(
-//     <Provider store={store}>
-//       {router}
-//     </Provider>,
-//     document.getElementById('app')
-// );
+render(
+    <Provider store={store}>
+      {router}
+    </Provider>,
+    document.getElementById('app')
+);
