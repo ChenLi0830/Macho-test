c0cdd6855538e4df5dbf970936163a11529d11ed
Chen Li <yichen.li0830@gmail.com>
2017-02-09 16:15:47 -0800

    Implement banner

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -3,6 +3,7 @@ import { Button, Icon } from 'antd';
 import QueueAnim from 'rc-queue-anim';
 import TweenOne from 'rc-tween-one';
 import BannerAnim, { Element } from 'rc-banner-anim';
+import {Link} from 'rc-scroll-anim';
 import 'rc-banner-anim/assets/index.css';
 import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
 
@@ -10,13 +11,17 @@ const BgElement = Element.BgElement;
 class Banner extends React.Component {
   render() {
     const childrenData = [
-      { children: { title: { children: 'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png' }, content: { children: '一个高效的页面动画解决方案' }, button: { children: 'Learn More' } } },
-      { children: { title: { children: 'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png' }, content: { children: '一个高效的页面动画解决方案' }, button: { children: 'Learn More' } } },
+      { children: { title: { children: "Sem Tortor Mollis Aenean" }, content: { children: 'Donec' +
+      ' id elit mi non porta gravida et eget metus. Vivamus sagittis lacus vel augue laoreet' +
+      ' rutrum'}, button: { children: 'EXPLORE' } } },
+      { children: { title: { children: 'Hoy es un buen dia' }, content: { children: 'Donec' +
+      ' id elit mi non porta gravida et eget metus. Vivamus sagittis lacus vel augue laoreet' +
+      ' rutrum'}, button: { children: 'Learn More' } } },
     ];
     const defaultImg = [
-      'https://zos.alipayobjects.com/rmsportal/hzPBTkqtFpLlWCi.jpg',
-      'https://zos.alipayobjects.com/rmsportal/xHxWkcvaIcuAdQl.jpg',
-    ]
+      'https://s3-us-west-2.amazonaws.com/macho.test/images/photo-1440439307159-c3afc8a8e4ff.jpeg',
+      'https://s3-us-west-2.amazonaws.com/macho.test/images/photo-1427104227401-94b390b378b0.jpeg',
+    ];
     const childrenToRender = childrenData.map((item, i) => {
       const children = item.children;
       const isImg = children.title.children
@@ -32,34 +37,36 @@ class Banner extends React.Component {
             backgroundImage: `url(${defaultImg[i]})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-          }}
-        />
+          }}>
+        </BgElement>
+          
         <QueueAnim
           type={['bottom', 'top']} delay={200}
-          className={`${this.props.className}-title`} key="text"
+          className={`banner1-title`} key="text"
           style={children.wrapper && children.wrapper.style}
         >
-          <span
-            className="logo" key="logo"
-            style={children.title.style}
-          >
-            {isImg ?
-              (<img width="100%" src={children.title.children} />) :
-              children.title.children}
-          </span>
           <p
-            key="content"
+              className="title"
+          >
+            {children.title.children}
+          </p>
+          
+          <p
+            className="content"
             style={children.content.style}
           >
             {children.content.children}
           </p>
-          <Button
-            type="ghost"
-            key="button"
-            style={children.button.style}
-          >
-            {children.button.children}
-          </Button>
+          <Link to={"Content1"} toHash={false}>
+            <Button
+                type="ghost"
+                key="button"
+                className="capitalize"
+                style={children.button.style}
+            >
+              {children.button.children}
+            </Button>
+          </Link>
         </QueueAnim>
       </Element>);
     });
@@ -76,7 +83,7 @@ class Banner extends React.Component {
           </BannerAnim>
           <TweenOne
             animation={{ y: '-=20', yoyo: true, repeat: -1, duration: 1000 }}
-            className={`${this.props.className}-icon`}
+            className={`banner1-icon`}
             style={{ bottom: 40 }}
             key="icon"
           >
