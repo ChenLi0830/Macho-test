9809e818bc835bc70ee5922a7259bbac5d12cce8
Chen Li <yichen.li0830@gmail.com>
2017-02-10 17:09:50 -0800

    Improve footer style

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -5,7 +5,7 @@ import {Row, Col} from 'antd';
 const Footer = (props) => {
   const footerContent = ["Product", "Works", "Team", "About", "Contact Us"];
   const children = footerContent.map((content, i) => (
-      <Col xs={12} sm={4} key={i}>
+      <Col xs={12} sm={4} key={i} className="pointer">
         {content}
       </Col>
   ));
@@ -15,7 +15,7 @@ const Footer = (props) => {
     "https://s3-us-west-2.amazonaws.com/macho.test/images/google.png",
   ];
   const socialChildren = socials.map((img, i) => (
-      <Col xs={12} sm={4} key={i}>
+      <Col xs={12} sm={4} key={i} className="pointer">
         <img src={img} className="social-img"/>
       </Col>
   ));
@@ -27,16 +27,16 @@ const Footer = (props) => {
           playScale={0.2}
           hideProps={{copyright: {reverse: true}}}>
         <Row>
-          <Col xs={{span: 24}} sm={{span: 20, offset: 2}} className="logo">
-            <div className="capitalize">Macho</div>
+          <Col xs={{span: 24, offset: 1}} sm={{span: 20, offset: 2}} className="logo">
+            <div className="capitalize pointer">Macho</div>
           </Col>
         </Row>
         
         <Row>
-          <Col xs={{span: 24}} sm={{span: 10, offset: 2}} className="content">
+          <Col xs={{span: 22, offset: 1}} sm={{span: 10, offset: 2}} className="content">
             {children}
           </Col>
-          <Col xs={{span: 24}} sm={{span: 6, offset: 6}} md={{span: 4, offset: 8}}
+          <Col xs={{span: 24, offset: 1}} sm={{span: 6, offset: 6}} md={{span: 4, offset: 8}}
                className="content">
             {socialChildren}
           </Col>
